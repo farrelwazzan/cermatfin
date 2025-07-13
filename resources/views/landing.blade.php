1ca22caa49056cde34172f42ea036ea28f2ac549
farrelwazzan <farelwazzan@gmail.com>
2025-07-13 15:22:52 +0700

    navbar saja

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <div class="" style="margin-left: 80;">
+        <div class="container text-center bg-primary">
+            <div class="row mt-4 align-items-center">
+                <div class="col-2 caveat-text">
+                    <h1>CermatF1n</h1>
+                </div>
+                <div class="col-5">
+                    
+                </div>
+                <div class="col-5">
+                    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0"> 
+                        <li><a href="#" class="nav-link px-4 link-dark montserrat-text" style="font-size: 0.85rem;">BERANDA</a></li> 
+                        <li><a href="#" class="nav-link px-4 link-dark montserrat-text" style="font-size: 0.85rem;">FITUR</a></li> 
+                        <li><a href="#" class="nav-link px-4 link-dark montserrat-text" style="font-size: 0.85rem;">DAFTAR</a></li> 
+                        <li><a href="#" class="nav-link px-4 link-dark montserrat-text" style="font-size: 0.85rem;">TESTIMONE</a></li> 
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
